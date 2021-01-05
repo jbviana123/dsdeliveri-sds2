afa8e85042339caa332f85519e59c18832e0b822
jbviana123 <joaobvianna@hotmail.com>
2021-01-05 10:59:01 -0300

    project created

---
+++ backend/src/test/java/com/devsuperior/dsdeliveri/DsdeliveriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliveri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
